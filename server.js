3539f385855ee74b0498d8596f73bcd9304ff85f
Moldovan Andrei <moldovanandrei8399@gmail.com>
2019-03-27 00:30:10 +0000

    sdf

--- server.js
+++ server.js
@@ -53,7 +53,6 @@ app.get("/token", function(request, response) {
 });
 
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log("Express server listening on *:" + port);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
